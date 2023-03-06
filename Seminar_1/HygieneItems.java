376a99cd3800ecf154793bb4cdb5ddf9c2ee6e12
Pavel <Shestakov1996@yandex.ru>
2023-03-06 23:28:56 +0300

    Заполнил классс предметов гигиены

--- Seminar_1/HygieneItems.java
+++ Seminar_1/HygieneItems.java
@@ -1,5 +1,36 @@
 package Seminar_1;
 
-public class HygieneItems {
+public class HygieneItems extends Product {
+
+    protected int quantinpack;
+    public HygieneItems(String name, int price, int quantity, String units, int quantinpack) {
+        super(name, price, quantity, units);
+        this.quantinpack=quantinpack;
+    }
+    
+}
+
+class ToiletPaper extends HygieneItems{
+    private int quantlayer;
+    public ToiletPaper(String name, int price, int quantity, String units, int quantinpack, int quantlayer) {
+        super(name, price, quantity, units, quantinpack);
+        this.quantlayer=quantlayer;
+    }
+    public String getInfo() {
+        return String.format("Name: %s  Price: %d  Quantity: %d Units: %s QuantInPack: %d QuantLayer: %d",
+                this.name, this.price, this.quantity, this.units, this.quantinpack,this.quantlayer);
+    } 
+}
+
+class Masks extends HygieneItems{
+
+    public Masks(String name, int price, int quantity, String units, int quantinpack) {
+        super(name, price, quantity, units, quantinpack);
+    }
+
+    public String getInfo() {
+        return String.format("Name: %s  Price: %d  Quantity: %d Units: %s QuantInPack: %d ",
+                this.name, this.price, this.quantity, this.units, this.quantinpack);
+    }
     
 }
