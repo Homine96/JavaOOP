47a8221435612f5243648035534035b2c541d858
Pavel <Shestakov1996@yandex.ru>
2023-03-06 23:11:14 +0300

    Дополнил класс Program данными о напитках

--- Seminar_1/Program.java
+++ Seminar_1/Program.java
@@ -4,8 +4,15 @@ public class Program {
      public static void main(String[] args) {
         Bread hleb = new Bread("Rus", 12, 0, "kg", 0, "wer");
         System.out.println(hleb.getInfo());
+
         Eggs eggs1 = new Eggs("Eggs", 135, 10, "упаковка", 12, 9);
         System.out.println(eggs1.getInfo());
+
+        Milk milk = new Milk("Весёлый молочник", 70, 1, "поштучно", "1 литр", "2 месяца", "3.2 %");
+        System.out.println(milk.getInfo());
+
+        Limonade lime = new Limonade("Sprite", 150, 2, "бутылка", "Литр");
+
     }
         
     
