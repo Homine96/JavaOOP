a9df894a51e3ff18a0514741771df264155d5476
Pavel <Shestakov1996@yandex.ru>
2023-03-06 16:57:58 +0300

    Работа с классом Programm и классами FoodProducts and Product

--- Seminar_1/FoodProducts.java
+++ Seminar_1/FoodProducts.java
@@ -1,5 +1,50 @@
 package Seminar_1;
 
-public class FoodProducts {
+
+
+public class FoodProducts extends Product {
+
+    public FoodProducts(String name, int price, int quantity, String units, int shelflife) {
+        super(name, price, quantity, units);
+        this.shelflife=shelflife;
+    }
+    protected int shelflife;
+
+
+
+}
+
+class Bread extends FoodProducts {
+    public Bread(String name, int price, int quantity, String units, int shelflife, String typem) {
+        super(name, price, quantity, units, shelflife);
+        this.typem=typem;
+        //TODO Auto-generated constructor stub
+    }
+
+    protected String typem;
+
+    public String getInfo() {
+        return String.format("Name: %s  Price: %d  Quantity: %d Units: %s Shelflife: %d Typem: %s",
+                this.name, this.price, this.quantity, this.units, this.shelflife,this.typem);
+    }
+}
+
+
+class Eggs extends FoodProducts{
+
+    private int quantity2;
+
+    public Eggs(String name, int price, int quantity, String units, int shelflife, int quantity2) {
+        super(name, price, quantity, units, shelflife);
+        this.quantity2=quantity2;
+        //TODO Auto-generated constructor stub
+    }
+
+    public String getInfo() {
+        return String.format("Name: %s  Price: %d  Quantity: %d Units: %s Shelflife: %d Quantity2: %s",
+                this.name, this.price, this.quantity, this.units, this.shelflife,this.quantity2);
+    }
+
     
+
 }
--- Seminar_1/Product.java
+++ Seminar_1/Product.java
@@ -2,4 +2,53 @@ package Seminar_1;
 
 public class Product {
     
+    protected String name;
+    protected int price;
+    protected int quantity;
+    protected String units;
+
+
+    public Product( String name, int price, int quantity, String units){
+        this.name=name;
+        this.price=price;
+        this.quantity=quantity;
+        this.units=units;
+    }
+    //getters
+    public String getName(){
+        return this.name;
+    }
+
+    public int getPrice(){
+        return this.price;
+    }
+
+    public int getQuantity(){
+        return this.quantity;
+    }
+
+    public String getUnits(){
+        return this.units;
+    }
+
+    public String getInfo() {
+        return String.format("Name: %s  Price: %d  Quantity: %d Units: %s",
+                this.name, this.price, this.quantity, this.units);
+    }
+
+    //setters
+
+    public void setName(String name){
+        this.name=name;
+    }
+    
+    public void setPrice(int price){
+        this.price=price;
+    }
+    public void setQuantity(int quantity){
+        this.quantity=quantity;
+    }
+    public void setUnits(String units){
+        this.units=units;
+    }
 }
