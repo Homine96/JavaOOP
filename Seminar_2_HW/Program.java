d24002c862218892a150430f45b167712e948f47
Pavel <Shestakov1996@yandex.ru>
2023-03-11 19:29:43 +0300

    modified

--- Seminar_2_HW/Program.java
+++ Seminar_2_HW/Program.java
@@ -1,15 +1,35 @@
 package Seminar_2_HW;
 
-import Seminar_2_HW.Home.Dog;
+import Seminar_2_HW.Abstract.Animal;
+import Seminar_2_HW.Abstract.HomeAnimal;
+import Seminar_2_HW.Abstract.WildAnimal;
+import Seminar_2_HW.Animal.*;
+import Seminar_2_HW.Birds.Aist;
+import Seminar_2_HW.Birds.Hen;
 
 public class Program {
     public static void main(String[] args) {
         
-        Dog grusha = new Dog(60, 10, "голубые", "Груша", "Корги", "Вакцинирован", "Коричневая", "17.04.2022", "Дрессированный");
+        Dog grusha = new Dog();
         System.out.println(grusha.getInfo()); 
         grusha.makeSound();
         grusha.showAffection();
         grusha.train();
+
+        WildAnimal volk = new Wolf();
+        System.out.println(volk.getInfo());
+        volk.makeSound();
+
+        WildAnimal tigr = new Tiger();
+        System.out.println(tigr.getInfo());
+        tigr.makeSound();
         
+        Hen kura = new Hen();
+        System.out.println(kura.getInfo());
+        kura.fly();
+        kura.makeSound();
+
+        Aist ad = new Aist();
+        ad.fly();
     }
 }
