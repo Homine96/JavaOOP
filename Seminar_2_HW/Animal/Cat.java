a811fc83137a6c9cdd60e59fc8030edb59e0cefa
Pavel <Shestakov1996@yandex.ru>
2023-03-11 16:10:58 +0300

    Modified Cat

---
+++ Seminar_2_HW/Animal/Cat.java
@@ -0,0 +1,31 @@
+package Seminar_2_HW.Animal;
+
+import Seminar_2_HW.Abstract.HomeAnimal;
+
+public class Cat extends HomeAnimal{
+
+    private String wool;
+
+    public Cat(int height, int weight, String eyeColor, String name, String breed, String vaccine, String colorwool,
+            String date, String wool) {
+        super(height, weight, eyeColor, name, breed, vaccine, colorwool, date);
+        this.wool = wool;
+        
+    }
+
+    public String getInfo() {
+        return String.format(" %s Wool: %s ",
+                super.getInfo(), this.wool );
+    }
+
+    @Override
+    public void showAffection() {
+        System.out.println("Мурчит");;
+    }
+
+    @Override
+    public void makeSound() {
+       System.out.println("Мяу!");;
+    }
+    
+}
